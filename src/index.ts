cad33f6a852538f04bc183e2f506f40ae6629504
12LuckyDev <12luckydev@gmail.com>
2021-01-27 12:33:03 +0100

    addValue func

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export const Test = (text: string) => `Test ${text}`;
+/**
+ * Return array copy with added value 
+ * @param array Original array
+ * @param value Value to be added
+ */
+export const addValue = <T>(array: Array<T>, value: T): Array<T> => [...array, value];
