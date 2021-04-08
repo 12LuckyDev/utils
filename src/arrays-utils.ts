12083847305bc296b894715b18455a80437a4f71
12LuckyDev <12luckydev@gmail.com>
2021-04-08 16:48:38 +0200

    Change Array<T> to T[]

--- src/arrays-utils.ts
+++ src/arrays-utils.ts
@@ -3,14 +3,14 @@
  * @param a First array
  * @param b Second array
  */
-export const merge = <T>(a: Array<T>, b: Array<T>): Array<T> => [...a, ...b];
+export const merge = <T>(a: T[], b: T[]): T[] => [...a, ...b];
 
 /**
  * Returns array copy with added value
  * @param array Original array
  * @param value Value to be added
  */
-export const add = <T>(array: Array<T>, ...values: Array<T>): Array<T> => merge(array, values);
+export const add = <T>(array: T[], ...values: T[]): T[] => merge(array, values);
 
 /**
  * Returns array copy with changed value at given index
@@ -18,8 +18,7 @@ export const add = <T>(array: Array<T>, ...values: Array<T>): Array<T> => merge(
  * @param value New value
  * @param index Index for new value
  */
-export const editAt = <T>(array: Array<T>, value: T, index: number): Array<T> =>
-  array.map((v, i) => (i === index ? value : v));
+export const editAt = <T>(array: T[], value: T, index: number): T[] => array.map((v, i) => (i === index ? value : v));
 
 /**
  * Returns array copy with changed value located by property value
@@ -28,7 +27,7 @@ export const editAt = <T>(array: Array<T>, value: T, index: number): Array<T> =>
  * @param value Value with which objects will be deleted
  * @param propValue Property value after which the object is located
  */
-export const editByProp = <T, K extends keyof T>(array: Array<T>, value: T, key: K, propValue?: T[K]): Array<T> => {
+export const editByProp = <T, K extends keyof T>(array: T[], value: T, key: K, propValue?: T[K]): T[] => {
   const propV = propValue ?? value[key];
   return array.map((v) => (v[key] === propV ? value : v));
 };
@@ -38,7 +37,7 @@ export const editByProp = <T, K extends keyof T>(array: Array<T>, value: T, key:
  * @param array Original array
  * @param index Index of value to remove
  */
-export const removeAt = <T>(array: Array<T>, index: number): Array<T> => array.filter((v, i) => i !== index);
+export const removeAt = <T>(array: T[], index: number): T[] => array.filter((v, i) => i !== index);
 
 /**
  * Returns an array to be deconstructed with the value of the given index and array copy without value from given index
@@ -46,7 +45,7 @@ export const removeAt = <T>(array: Array<T>, index: number): Array<T> => array.f
  * @param index Index of value to remove
  * @returns [value, new array without value from given index]
  */
-export const popByIndex = <T>(array: Array<T>, index: number): [T, Array<T>] => [array[index], removeAt(array, index)];
+export const popByIndex = <T>(array: T[], index: number): [T, T[]] => [array[index], removeAt(array, index)];
 
 /**
  * Returns array copy without values with specific properties value
@@ -54,7 +53,7 @@ export const popByIndex = <T>(array: Array<T>, index: number): [T, Array<T>] => 
  * @param key Property name of value to compare
  * @param propValue Value with which objects will be deleted
  */
-export const removeByProp = <T, K extends keyof T>(array: Array<T>, key: K, propValue: T[K]): Array<T> =>
+export const removeByProp = <T, K extends keyof T>(array: T[], key: K, propValue: T[K]): T[] =>
   array.filter((v) => v[key] !== propValue);
 
 /**
@@ -66,19 +65,17 @@ export const removeByProp = <T, K extends keyof T>(array: Array<T>, key: K, prop
  * @param singleValue The flag deciding whether a single value from the value array is expected
  */
 export const popByProp = <T, K extends keyof T, B extends boolean>(
-  array: Array<T>,
+  array: T[],
   key: K,
   propValue: T[K],
   singleValue: B = true as B,
-): B extends true ? [T | undefined, Array<T>] : [Array<T>, Array<T>] => {
+): B extends true ? [T | undefined, T[]] : [T[], T[]] => {
   const newArray = removeByProp(array, key, propValue);
   if (singleValue) {
     const v = array.find((v) => v[key] === propValue);
-    return [v, newArray] as B extends true ? [T | undefined, Array<T>] : [Array<T>, Array<T>];
+    return [v, newArray] as B extends true ? [T | undefined, T[]] : [T[], T[]];
   }
-  return [array.filter((v) => v[key] === propValue), newArray] as B extends true
-    ? [T | undefined, Array<T>]
-    : [Array<T>, Array<T>];
+  return [array.filter((v) => v[key] === propValue), newArray] as B extends true ? [T | undefined, T[]] : [T[], T[]];
 };
 
 /**
@@ -87,7 +84,7 @@ export const popByProp = <T, K extends keyof T, B extends boolean>(
  * @param key Property name of value to compare
  * @param propValues Values with which objects will be deleted
  */
-export const removeByProps = <T, K extends keyof T>(array: Array<T>, key: K, propValues: Array<T[K]>): Array<T> =>
+export const removeByProps = <T, K extends keyof T>(array: T[], key: K, propValues: Array<T[K]>): T[] =>
   array.filter((v) => !propValues.includes(v[key]));
 
 /**
@@ -95,4 +92,4 @@ export const removeByProps = <T, K extends keyof T>(array: Array<T>, key: K, pro
  * @param n New array lenght
  * @param callback Map callback function
  */
-export const nMap = <T>(n: number, callback: (i: number) => T): Array<T> => [...new Array(n).keys()].map(callback);
+export const nMap = <T>(n: number, callback: (i: number) => T): T[] => [...new Array(n).keys()].map(callback);
