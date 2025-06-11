a41fe0a14252bf1b6cc7145eb4d5cf932123dd19
12LuckyDev <12luckydev@gmail.com>
2025-06-11 12:59:58 +0200

    Refactor for forEachProp

--- src/object-utils.ts
+++ src/object-utils.ts
@@ -45,8 +45,10 @@ export const mapToObjectUsing = <T, K extends keyof T>(
  * @param obj Input object
  * @param handler Handler with action
  */
-export const forEachProp = (obj: Record<string, unknown>, handler: (prop?: unknown, key?: string) => void): void => {
-  Object.keys(obj).forEach((k) => handler(obj[k], k));
+export const forEachProp = (obj: Record<string, unknown>, handler: (prop: unknown, key: string) => void): void => {
+  for (let k in obj) {
+    handler(obj[k], k);
+  }
 };
 
 /**
